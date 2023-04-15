d2c2107da5f91183686d952f69a9b23be094d1cf
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-04-15 11:08:39 -0400

    initial

---
+++ MobSpawner.gd
@@ -0,0 +1,65 @@
+class_name MobSpawner
+extends Node
+
+@onready var mob_path = $MobPath
+@onready var mob_spawn_point = $MobPath/MobSpawnPoint
+
+
+var _Monster: PackedScene = preload("res://characters/Monster.tscn")
+
+var all_textures: Dictionary = {"bodies":[], "eyes":[]}
+
+func _ready():
+	load_textures_to_dict(all_textures)
+	set_path()
+
+
+func set_path():
+	var curve = mob_path.curve
+	var points = [Vector2.ZERO, Vector2(get_viewport().size.x, 0), Vector2(get_viewport().size), Vector2(0, get_viewport().size.y)]
+	points.append(Vector2.ZERO) # close the path
+	for point in points:
+		curve.add_point(point)
+
+
+func spawn() -> Monster:
+	var mob = _Monster.instantiate() as Monster
+#	add_child(mob)
+	mob.textures["body"] = all_textures["bodies"][0]#.pick_random()
+	mob.textures["eye"] = all_textures["eyes"][0]#.pick_random()
+
+	mob_spawn_point.progress_ratio = randf()
+	mob.position = mob_spawn_point.position
+	mob.rotation = mob_spawn_point.rotation + PI/2 + randf_range(-PI / 4, PI / 4)
+
+	return mob
+
+
+# Load PNG files
+func load_textures_to_dict(dict: Dictionary):
+	var path = "res://assets/images/"
+	var filenames: Array[String] = dir_contents(path)
+	for file in filenames:
+		if file.begins_with("body"):
+			dict["bodies"].append(load(path + file))
+		if file.begins_with("eye"):
+			dict["eyes"].append(load(path + file))
+
+
+func dir_contents(path) -> Array[String]:	
+	var files: Array[String] = []
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name: String = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				if file_name.to_lower().ends_with("png"):
+					files.append(file_name)
+				file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+		
+	return files
